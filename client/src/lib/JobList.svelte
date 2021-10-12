3c880cc49e22d97904e69836cf7c7e099669c51a
Ryota Mizumaki <statham.programming@gmail.com>
2021-10-12 09:13:35 +0900

    add job list

---
+++ client/src/lib/JobList.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import type { Job } from "src/logics/Job";
+  export let items: Job[];
+</script>
+
+<div>
+  <ul>
+    {#each items as job}
+      <li>{job.name}</li>
+    {/each}
+  </ul>
+</div>
+
+<style>
+</style>
---
+++ client/src/logics/Job/index.ts
@@ -0,0 +1,49 @@
+export type Command = string
+
+export type Job = SingleJob | ChainJob | ClusterJob;
+
+export type SingleJob = {
+  command: Command;
+} & JobBase;
+
+export type ChainJob = {
+  chainJobs: {
+    [index: number]: Job;
+  };
+} & JobBase;
+
+export type ClusterJob = {
+  jobCluster: Job[];
+} & JobBase;
+
+export const JobStatus = {
+  succeed: 'succeed',
+  failed: 'failed',
+  pending: 'pending',
+  beforeStart: 'before-start',
+} as const;
+type JobStatus = typeof JobStatus[keyof typeof JobStatus];
+
+type JobBase = {
+  id: string;
+  name: string;
+  createdAt: Date;
+  onSuccess?: Command;
+  onFailed?: Command;
+} & (
+  | {
+      status: 'succeed' | 'failed';
+      startedAt: Date;
+      endedAt: Date;
+    }
+  | {
+      status: 'pending';
+      startedAt: Date;
+      endedAt?: undefined;
+    }
+  | {
+      status: 'before-start';
+      startedAt?: undefined;
+      endedAt?: undefined;
+    }
+);
