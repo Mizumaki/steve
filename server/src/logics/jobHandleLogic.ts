5e0a6f2052bc6a4ed8bb4514df89a8120693dd27
Ryota Mizumaki <statham.programming@gmail.com>
2021-10-27 09:18:13 +0900

    make handleJob logic testable

--- server/src/logics/jobHandleLogic.ts
+++ server/src/logics/jobHandleLogic.ts
@@ -1,6 +1,122 @@
 import { Job, JobBehaviorOnFailed, JobStatus, JobType } from '~/entity/Job';
 import { JobHistoryRepositoryInterface } from '~/repository/JobHistory';
-import { CommandError, runCommand } from '~/utils/runCommand';
+import { CommandError, runCommand as _runCommand } from '~/utils/runCommand';
+
+const handleJobGen = ({
+  updateStatusToRunning,
+  updateStatusToPending,
+  updateStatusToSuccess,
+  updateStatusToFailed,
+  runCommand,
+}: {
+  updateStatusToRunning: (jobId: string) => Promise<void>;
+  updateStatusToPending: (jobId: string) => Promise<void>;
+  updateStatusToSuccess: (jobId: string) => Promise<void>;
+  updateStatusToFailed: (jobId: string) => Promise<void>;
+  runCommand: (
+    command: string
+  ) => Promise<{
+    error: CommandError | undefined;
+    stdout: string;
+    stderr: string;
+  }>;
+}) => async (job: Job): Promise<{ error: undefined | CommandError }> => {
+  const handleJob = handleJobGen({
+    updateStatusToRunning,
+    updateStatusToPending,
+    updateStatusToSuccess,
+    updateStatusToFailed,
+    runCommand,
+  });
+  switch (job.type) {
+    case JobType.single: {
+      await updateStatusToRunning(job.id);
+      const { error, stdout, stderr } = await runCommand(job.command);
+      if (error) {
+        await updateStatusToFailed(job.id);
+        if (job.onFailed) {
+          await runCommand(job.onFailed).catch(e => {
+            // TODO: Handle log well
+            console.error(`The 'onFailed' for ${job.id} failed: ${(e as unknown) as string}`);
+            throw e;
+          });
+        }
+        return { error };
+      }
+      await updateStatusToSuccess(job.id);
+      if (job.onSuccess) {
+        // TODO: pass these to job.onSuccess
+        console.info(`stdout: ${stdout}, stderr: ${stderr}`);
+        await runCommand(job.onSuccess).catch(e => {
+          // TODO: Handle log well
+          console.error(`The 'onSuccess' for ${job.id} failed: ${(e as unknown) as string}`);
+          throw e;
+        });
+      }
+      return { error: undefined };
+    }
+    case JobType.chain: {
+      await updateStatusToRunning(job.id);
+      const failedJobs: Job[] = [];
+      chainLoop: for (const j of job.chainJobs) {
+        const { error } = await handleJob(j);
+        if (error) {
+          failedJobs.push(j);
+          switch (job.whenOneOfChainFailed) {
+            case JobBehaviorOnFailed.stop:
+              break chainLoop;
+            case JobBehaviorOnFailed.fail:
+              break chainLoop;
+            case JobBehaviorOnFailed.skip:
+              continue chainLoop;
+          }
+        }
+      }
+
+      if (failedJobs.length === 0) {
+        await updateStatusToSuccess(job.id);
+      } else {
+        switch (job.whenOneOfChainFailed) {
+          case JobBehaviorOnFailed.stop:
+            await updateStatusToPending(job.id);
+            break;
+          case JobBehaviorOnFailed.fail:
+            await updateStatusToFailed(job.id);
+            break;
+          case JobBehaviorOnFailed.skip:
+            await updateStatusToSuccess(job.id);
+            break;
+        }
+      }
+
+      if (job.onEnd) {
+        // TODO: Pass success/failed job ids
+        await runCommand(job.onEnd).catch(e => {
+          // TODO: Handle log well
+          console.error(`The 'onEnd' for ${job.id} failed: ${(e as unknown) as string}`);
+          throw e;
+        });
+      }
+      return { error: undefined };
+    }
+    case JobType.cluster: {
+      // TODO(PR): clusterJob のケースも追加
+      await updateStatusToRunning(job.id);
+      const jobResults = await Promise.all(job.jobCluster.map(j => handleJob(j)));
+      await updateStatusToSuccess(job.id);
+      if (job.onEnd) {
+        console.log({ jobResults });
+        // TODO: Pass success/failed job ids
+        await runCommand(job.onEnd).catch(e => {
+          // TODO: Handle log well
+          console.error(`The 'onEnd' for ${job.id} failed: ${(e as unknown) as string}`);
+          throw e;
+        });
+      }
+      return { error: undefined };
+    }
+  }
+};
 
 export const jobHandleLogic = ({
   jobHistoryRepository,
@@ -15,96 +131,13 @@ export const jobHandleLogic = ({
   const updateStatusToFailed = (jobId: string) =>
     jobHistoryRepository.updateJob(jobId, { status: JobStatus.failed, endedAt: new Date() });
 
-  const handleJob = async (job: Job): Promise<{ error: undefined | CommandError }> => {
-    switch (job.type) {
-      case JobType.single: {
-        await updateStatusToRunning(job.id);
-        const { error, stdout, stderr } = await runCommand(job.command);
-        if (error) {
-          await updateStatusToFailed(job.id);
-          if (job.onFailed) {
-            await runCommand(job.onFailed).catch(e => {
-              // TODO: Handle log well
-              console.error(`The 'onFailed' for ${job.id} failed: ${(e as unknown) as string}`);
-              throw e;
-            });
-          }
-          return { error };
-        }
-        await updateStatusToSuccess(job.id);
-        if (job.onSuccess) {
-          // TODO: pass these to job.onSuccess
-          console.info(`stdout: ${stdout}, stderr: ${stderr}`);
-          await runCommand(job.onSuccess).catch(e => {
-            // TODO: Handle log well
-            console.error(`The 'onSuccess' for ${job.id} failed: ${(e as unknown) as string}`);
-            throw e;
-          });
-        }
-        return { error: undefined };
-      }
-      case JobType.chain: {
-        await updateStatusToRunning(job.id);
-        const failedJobs: Job[] = [];
-        chainLoop: for (const j of job.chainJobs) {
-          const { error } = await handleJob(j);
-          if (error) {
-            failedJobs.push(j);
-            switch (job.whenOneOfChainFailed) {
-              case JobBehaviorOnFailed.stop:
-                break chainLoop;
-              case JobBehaviorOnFailed.fail:
-                break chainLoop;
-              case JobBehaviorOnFailed.skip:
-                continue chainLoop;
-            }
-          }
-        }
-
-        if (failedJobs.length === 0) {
-          await updateStatusToSuccess(job.id);
-        } else {
-          switch (job.whenOneOfChainFailed) {
-            case JobBehaviorOnFailed.stop:
-              await updateStatusToPending(job.id);
-              break;
-            case JobBehaviorOnFailed.fail:
-              await updateStatusToFailed(job.id);
-              break;
-            case JobBehaviorOnFailed.skip:
-              await updateStatusToSuccess(job.id);
-              break;
-          }
-        }
-
-        if (job.onEnd) {
-          // TODO: Pass success/failed job ids
-          await runCommand(job.onEnd).catch(e => {
-            // TODO: Handle log well
-            console.error(`The 'onEnd' for ${job.id} failed: ${(e as unknown) as string}`);
-            throw e;
-          });
-        }
-        return { error: undefined };
-      }
-      case JobType.cluster: {
-        // TODO(PR): clusterJob のケースも追加
-        await updateStatusToRunning(job.id);
-        const jobResults = await Promise.all(job.jobCluster.map(j => handleJob(j)));
-        await updateStatusToSuccess(job.id);
-        if (job.onEnd) {
-          console.log({ jobResults });
-          // TODO: Pass success/failed job ids
-          await runCommand(job.onEnd).catch(e => {
-            // TODO: Handle log well
-            console.error(`The 'onEnd' for ${job.id} failed: ${(e as unknown) as string}`);
-            throw e;
-          });
-        }
-        return { error: undefined };
-      }
-    }
-  };
+  const handleJob = handleJobGen({
+    updateStatusToRunning,
+    updateStatusToPending,
+    updateStatusToSuccess,
+    updateStatusToFailed,
+    runCommand: _runCommand,
+  });
 
   await handleJob(rootJob);
 };
